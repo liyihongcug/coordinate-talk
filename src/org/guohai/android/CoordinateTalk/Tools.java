af958d7c6282b8741610a3f2a0e4aacbc3d9fbe5
guohai <guohai@gmail.com>
2011-01-31 11:14:20 +0800

    create pro

---
+++ src/org/guohai/android/CoordinateTalk/Tools.java
@@ -0,0 +1,14 @@
+package org.guohai.android.CoordinateTalk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Display;
+
+public class Tools {
+	
+	public static int GetResolution()
+	{
+		//Display dis = getWindowManager().getDefaultDisplay();
+		return 0;
+	}
+}
