34321c52758006486f666c7f5ce97815f191060d
guoyunfeng <guoyunfeng@gmail.com>
2011-02-16 11:15:04 +0800

    UI???UI????

---
+++ src/org/guohai/android/cta/CoordinateTalk.java
@@ -0,0 +1,267 @@
+/**
+ * 此程序为开源程序，遵循GPLv3版本发布，并受其保护。
+ * (GPLv3 http://www.gnu.org/licenses/gpl.html)
+ * Copyright 2011 by H!Guo
+ */
+package org.guohai.android.cta;
+
+import java.util.List;
+
+import android.content.*;
+import android.provider.*;
+import android.app.*;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.*;
+import android.telephony.NeighboringCellInfo;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.util.Log;
+import android.view.*;
+
+import org.guohai.android.cta.bll.*;
+import org.guohai.android.cta.model.MessageInfo;
+import org.guohai.android.cta.model.ParseGpsInfo;
+import org.guohai.android.cta.utility.GPSUtilities;
+import org.guohai.android.cta.utility.Tools;
+
+/**
+ * Main views 
+ * @author H!Guo 
+ *
+ */
+public class CoordinateTalk extends Activity {
+	private static final String TAG="CoordinateTalk";
+
+    private TextView textCoordinate;
+    private Button btnRefer;
+    private Button btnTest;
+    private EditText editMessage;
+    private Button btnWhere;
+    private TextView textAddress;
+    
+    
+    private GPSUtilities gps;
+    private static final int MENU_CONFIG=0;
+    private static final int MENU_HELP=2;
+    private static final int MENU_EXIT=3;
+    
+    private Handler mMainHandler;
+    private static boolean theanState=false;
+    //private ChildThread childThread;
+    
+    //private boolean gpsIsOpen = false;
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        findViews();
+        setListeners();       
+        inita();          	
+    }
+    
+    public void onDestory(){
+    	super.onDestroy();
+    	//mChildHandler.getLooper().quit();
+    	theanState=false;
+    	mMainHandler.removeCallbacks(runnable);
+    }
+    
+    protected void onSopt()
+    {
+    	super.onStop();
+    	gps.pauseGetLocation();
+    	mMainHandler.removeCallbacks(runnable);
+    }
+    
+    /** 当程序重新开始的时候 */
+    @Override
+    protected void onResume(){
+    	super.onResume();    	
+    	gps.getLocation();
+    	mMainHandler.postDelayed(runnable, 1000);  
+    }
+    
+    /** 当程序暂停的时候 */
+    @Override
+    protected void onPause(){
+    	super.onPause();
+    	gps.pauseGetLocation();
+    	mMainHandler.removeCallbacks(runnable);
+    }
+    
+    /** Find all views */
+    private void findViews(){
+    	textCoordinate = (TextView)findViewById(R.id.coordinate);
+    	textAddress = (TextView)findViewById(R.id.my_address);
+    	editMessage = (EditText) findViewById(R.id.editText1);
+    	btnRefer = (Button)findViewById(R.id.button_refer);
+    	btnWhere = (Button)findViewById(R.id.where_am_i);
+    	btnTest = (Button)findViewById(R.id.button_test);  
+    }
+    
+    private void setListeners() {
+    	btnRefer.setOnClickListener(btnReferBMI);
+    	btnWhere.setOnClickListener(btnWhereBMI);
+    	btnTest.setOnClickListener(btnTestBMI);
+    }
+    
+    private Button.OnClickListener btnTestBMI = new Button.OnClickListener()
+    {
+    	public void onClick(View v)
+        {
+    		//gps.CellularPhone();
+    		TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+			GsmCellLocation gcl = (GsmCellLocation) tm.getCellLocation();
+			String a=gcl.getCid()+","+gcl.getLac()+"|";
+			List<NeighboringCellInfo> listNbInfo = tm.getNeighboringCellInfo();
+			int count=listNbInfo.size();
+			for(int i=0;i<count;i++){
+				a+= listNbInfo.get(i).getCid()+","+listNbInfo.get(i).getLac()+","+listNbInfo.get(i).getRssi()+"|";//取邻居小区号
+			}
+			Toast.makeText(CoordinateTalk.this, a, Toast.LENGTH_LONG).show();
+        }    	
+    };
+    
+    
+    private Button.OnClickListener btnWhereBMI = new Button.OnClickListener()
+    {
+    	public void onClick(View v)
+        {
+    		PostSiteData httpRest = new PostSiteData();
+			ParseGpsInfo parseInfo = new ParseGpsInfo();
+			parseInfo.Latitude = gps.Latitude;
+			parseInfo.Longitude = gps.Longitude;
+			parseInfo.SendAccount = Tools.GetPhoneImei(getApplicationContext());
+			if(parseInfo.Latitude==0 || parseInfo.Longitude==0){
+    			Toast.makeText(CoordinateTalk.this, "定位中..", Toast.LENGTH_SHORT).show();
+    			return ;
+			}
+			String Result = httpRest.GetParse(parseInfo);
+			textAddress.setText(Result);
+        }        
+    };
+    
+    
+    private Button.OnClickListener btnReferBMI = new Button.OnClickListener()
+    {
+        public void onClick(View v)
+        {
+        	Log.v(TAG, "维");
+			PostSiteData httpRest = new PostSiteData();
+    		MessageInfo message =new MessageInfo();
+    		message.Altitude = gps.Altitude;
+    		message.Latitude = gps.Latitude;
+    		message.Longitude = gps.Longitude;
+    		message.Note = editMessage.getText().toString();
+    		if(message.Note.length()==0){
+    			Toast.makeText(CoordinateTalk.this, "发送内容不能为空", Toast.LENGTH_SHORT).show();
+    			return ;
+    		}
+    		message.SendAccount=Tools.GetPhoneImei(getApplicationContext());
+			if(message.Latitude==0 || message.Longitude==0){
+    			Toast.makeText(CoordinateTalk.this, "定位中..", Toast.LENGTH_SHORT).show();
+    			return ;
+			}
+    		String Result = httpRest.AddMessage(message);
+    		Log.i(TAG,"["+Result+"]");
+    		if(Result.equals("200")){
+    			Log.i(TAG,Result+"1111");
+    			Toast.makeText(CoordinateTalk.this, "发送成功", Toast.LENGTH_SHORT).show();
+    			editMessage.setText("");
+    		}
+        }
+    };
+    
+    /** 创建菜单 */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+    	
+    	menu.add(0,MENU_CONFIG,0,R.string.menu_config)
+    		.setIcon(R.drawable.menu_sys_opt);
+    	//menu.add(0,MENU_SWAP_ACCOUNT,0,R.string.menu_swapaccount)
+		//	.setIcon(R.drawable.menu_logout);
+    	menu.add(0,MENU_HELP,0,R.string.menu_help)
+			.setIcon(R.drawable.help_menu_icon);
+    	menu.add(0,MENU_EXIT,0,R.string.menu_Exit)
+			.setIcon(R.drawable.exit_menu_icon);
+		return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	super.onOptionsItemSelected(item);
+    	switch(item.getItemId()){
+    	case MENU_CONFIG:
+    		Intent intent = new Intent(); 
+    		intent.setClass(CoordinateTalk.this, Config.class);
+    		startActivity(intent);
+    		break;
+    	case MENU_HELP:
+    		new AlertDialog.Builder(CoordinateTalk.this)     
+    		.setTitle("About Coordinate Talk")     
+    		.setMessage("your device imei\n"+Tools.GetPhoneImei(getApplicationContext()))     
+    		.show();
+    		break;
+    	case MENU_EXIT:
+    		this.finish();
+    		break;
+    	}
+    	return true;
+    }
+    
+    /** 初始化 */
+    private void inita(){
+    	gps = new GPSUtilities(getApplicationContext());
+    	
+    	textCoordinate.setText("维度：" +  gps.Latitude+ "\n经度：" + gps.Longitude+"\n高度："+gps.Altitude);
+    	//接收子线程消息
+    	mMainHandler = new Handler();    		
+    	mMainHandler.postDelayed(runnable, 1000);  
+    	
+        if(gps.GPSDeviceIsOpen()){
+        	//textCoordinate.setText("true");
+        	//gpsIsOpen=true;
+	        if(gps.getLocation()){
+	        	//textCoordinate.setText("维度：" +  gps.Latitude+ "\n经度" + gps.Longitude);
+	        	theanState = true;	        
+	        }
+        }
+        else{
+        	new AlertDialog.Builder(CoordinateTalk.this)
+        		.setTitle(R.string.setting_gps_title)
+        		.setMessage(R.string.setting_gps_info)
+        		.setPositiveButton(R.string.gps_setting,
+        				new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+					    		Intent intent = new Intent(Settings.ACTION_SECURITY_SETTINGS);
+					    		startActivityForResult(intent,0);
+					    		
+
+							}
+						})
+				.setNegativeButton(R.string.jump_gps_setting, null)
+        		.show();
+
+        	textCoordinate.setText("不打开GPS设置，本程序的某些功能可能无法正常执行！");
+        }
+    }
+    
+     private Runnable runnable = new Runnable() {  
+         public void run() { 
+        	 Log.i(TAG,"Thread ChildThread run!");
+        	 if(theanState)
+        	 {        						
+	         	Message toMain = mMainHandler.obtainMessage();
+	         	toMain.obj = "维度：" +  gps.Latitude+ "\n经度：" + gps.Longitude+"\n高度："+gps.Altitude;
+	         	textCoordinate.setText((String)toMain.obj);  					
+        	 }
+        	 mMainHandler.postDelayed(runnable, 1000);  
+          }  
+    };     
+   
+}
---
+++ src/org/guohai/android/cta/bll/PostSiteData.java
@@ -0,0 +1,87 @@
+package org.guohai.android.cta.bll;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.guohai.android.cta.model.ParseGpsInfo;
+import org.guohai.android.cta.utility.HttpRest;
+import org.guohai.android.cta.model.GsmCellLocationInfo;
+import org.guohai.android.cta.model.LocationInfo;
+import org.guohai.android.cta.model.MessageInfo;
+import android.util.Log;
+import android.content.Context;
+
+public class PostSiteData {
+	private static final String TAG="CoordinateTalk";
+	/** 反向解析 地址  */
+	public String GetParse(ParseGpsInfo parm){		
+		
+		List <NameValuePair> dataList = new ArrayList <NameValuePair>();
+		dataList.add(new BasicNameValuePair("SendAccount",parm.SendAccount));
+		dataList.add(new BasicNameValuePair("Latitude", Double.toString(parm.Latitude)));
+		dataList.add(new BasicNameValuePair("Longitude",Double.toString(parm.Longitude)));
+		
+		String data = HttpRest.HttpPostClient("http://android.guohai.org/api/?fun=parse", dataList);
+		
+		if(data == null)
+		{
+			return "失败了over";
+		}
+		return data;		
+	}
+	
+	
+	/**
+	 * POST数据
+	 * @param parm
+	 * @return
+	 */
+	public LocationInfo FromGSMGetLocation(GsmCellLocationInfo parm,Context context)
+	{
+		LocationInfo location = new LocationInfo();		
+		
+		Log.i(TAG,"call_id="+parm.CellId+",location_area_code="+parm.LocationAreaCode+",mobile_country_code="+parm.MobileCountryCode+",mobile_network_code="+parm.MobileNetworkCode);
+		List <NameValuePair> dataList = new ArrayList <NameValuePair>();
+		dataList.add(new BasicNameValuePair("cid",Integer.toString(parm.CellId)));
+		dataList.add(new BasicNameValuePair("lac", Integer.toString(parm.LocationAreaCode)));
+		dataList.add(new BasicNameValuePair("mcc",Integer.toString(parm.MobileCountryCode)));
+		dataList.add(new BasicNameValuePair("mnc",Integer.toString(parm.MobileNetworkCode)));
+		dataList.add(new BasicNameValuePair("imei",""));
+		
+		String data = HttpRest.HttpPostClient("http://android.guohai.org/api/?fun=gsm", dataList);
+		
+		if(data == null)
+		{
+			return location;
+		}
+		
+		String[] sArray = data.split(",");
+		location.Latitude = Double.parseDouble(sArray[0]);
+		location.Longitude = Double.parseDouble(sArray[1]);
+		return location;	
+	}
+	
+	
+	/** 增加一   */
+	public String AddMessage(MessageInfo message)
+	{		
+		List <NameValuePair> dataList = new ArrayList <NameValuePair>();
+		dataList.add(new BasicNameValuePair("Note",message.Note));
+		dataList.add(new BasicNameValuePair("SendAccount",message.SendAccount));
+		dataList.add(new BasicNameValuePair("Latitude", Double.toString(message.Latitude)));
+		dataList.add(new BasicNameValuePair("Longitude",Double.toString(message.Longitude)));
+		dataList.add(new BasicNameValuePair("Altitude",Double.toString(message.Altitude)));
+		
+		String data = HttpRest.HttpPostClient("http://android.guohai.org/api/?fun=add", dataList);		
+		if(data == null)
+		{
+			return "失败了over";
+		}
+		return "200";
+	}
+	
+	
+}
