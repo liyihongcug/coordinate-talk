4026e0f723f6eb584fa4f8d448d4fc89168b5695
yangjiayi530 <yangjiayi530@gmail.com>
2011-02-19 17:25:35 +0800

    ????????????/sdcard/ctadata?

---
+++ src/org/guohai/android/cta/model/ParseGpsInfo.java
@@ -0,0 +1,13 @@
+/**
+ * 此程序为开源程序，遵循GPLv3版本发布，并受其保护。
+ * (GPLv3 http://www.gnu.org/licenses/gpl.html)
+ * Copyright 2011 by H!Guo
+ */
+package org.guohai.android.cta.model;
+/**
+ * Message info 
+ * @author H!Guo 
+ */
+public class ParseGpsInfo extends LocationInfo {
+	public String SendAccount;//发送人
+}
---
+++ src/org/guohai/android/cta/model/ResultInfo.java
@@ -0,0 +1,16 @@
+/**
+ * 此程序为开源程序，遵循GPLv3版本发布，并受其保护。
+ * (GPLv3 http://www.gnu.org/licenses/gpl.html)
+ * Copyright 2011 by CTA Group
+ */
+package org.guohai.android.cta.model;
+
+/**
+ * 程序返回信息
+ * @author H!Guo
+ *
+ */
+public class ResultInfo {
+	public int State=0;
+	public String Message="";
+}
